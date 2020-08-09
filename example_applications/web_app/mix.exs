eb6b79f5079185bc03d3dcbd5ac992d72af1f0ce
Alex Koutmos <akoutmos@gmail.com>
2020-08-08 22:56:24 -0400

    Checkpoint

--- example_applications/web_app/mix.exs
+++ example_applications/web_app/mix.exs
@@ -46,7 +46,8 @@ defmodule WebApp.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:prom_ex, path: "../"}
     ]
   end
 
