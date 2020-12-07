c66d96e123233620c900ccb2177aa4d5f1d36a11
Alex Koutmos <akoutmos@gmail.com>
2020-12-06 20:43:58 -0500

    Fixing test compile warning

--- test/support/test_app_router.ex
+++ test/support/test_app_router.ex
@@ -1,5 +1,13 @@
+defmodule TestApp.UserController do
+  use Phoenix.Controller, namespace: TestApp
+
+  def index(conn, _params) do
+    json(conn, %{some: "data"})
+  end
+end
+
 defmodule TestApp.Router do
   use Phoenix.Router
 
-  get("/users", UserController, :index)
+  get("/users", TestApp.UserController, :index)
 end
