ed295c1b6dbd9d8b78791d60a9188073fb926941
Alex Koutmos <akoutmos@gmail.com>
2020-12-07 22:44:07 -0500

    Updating to latest prom_ex version

--- example_applications/web_app/mix.exs
+++ example_applications/web_app/mix.exs
@@ -47,7 +47,7 @@ defmodule WebApp.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:prom_ex, github: "akoutmos/prom_ex", branch: :ecto_plugin}
+      {:prom_ex, "~> 0.1.9-alpha"}
       # {:prom_ex, path: "../../"}
     ]
   end
