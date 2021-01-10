d182a01b2c8100d2c626745052105d6123502a1a
Alex Koutmos <akoutmos@gmail.com>
2021-01-10 10:51:25 -0500

    Added ecto to dashboard list

--- example_applications/web_app/lib/web_app/prom_ex.ex
+++ example_applications/web_app/lib/web_app/prom_ex.ex
@@ -78,6 +78,7 @@ defmodule WebApp.PromEx do
       {:prom_ex, "application.json"},
       {:prom_ex, "beam.json"},
       {:prom_ex, "phoenix.json"},
+      {:prom_ex, "ecto.json"},
       {:prom_ex, "oban.json"}
 
       # Add your dashboard definitions here with the format: {:otp_app, "path_in_priv"}
