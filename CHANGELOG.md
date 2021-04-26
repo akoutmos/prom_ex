92ee7db7b785d98085def10ae9b52fcebd4c0808
Alex Koutmos <akoutmos@gmail.com>
2021-04-26 13:48:57 -0400

    Added test for cron ets flusher

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+- Added ETSCronFlusher GenServer to supervision tree to flush distribution metrics from ETS
+  to address ETS memory leak when metrics are not scraped at a regular interval.
+
 ## [1.0.1] - 2021-03-14
 
 ### Changed
---
+++ test/prom_ex/ets_cron_flusher_test.exs
@@ -0,0 +1,55 @@
+defmodule PromEx.ETSCronFlusherTest do
+  use ExUnit.Case, async: false
+
+  alias PromEx.Plugins.Phoenix
+  alias PromEx.Test.Support.Events
+
+  defmodule DefaultPromExSetUp do
+    use PromEx, otp_app: :prom_ex
+
+    @impl true
+    def plugins do
+      [{Phoenix, router: TestApp.Router}]
+    end
+  end
+
+  describe "ETSCronFlusherTest" do
+    test "should flush ETS metrics at the correct interval" do
+      start_supervised!(DefaultPromExSetUp)
+      original_timer_ref = get_timer_ref()
+
+      Events.execute_all(:phoenix)
+
+      assert length(get_metrics_table()) == 5
+      assert length(get_dist_table()) == 6
+
+      Events.execute_all(:phoenix)
+
+      assert length(get_metrics_table()) == 5
+      assert length(get_dist_table()) == 12
+
+      Process.sleep(8_000)
+
+      assert length(get_metrics_table()) == 11
+      assert get_dist_table() == []
+
+      new_timer_ref = get_timer_ref()
+      assert original_timer_ref != new_timer_ref
+      assert is_reference(new_timer_ref)
+    end
+  end
+
+  defp get_dist_table do
+    :ets.tab2list(PromEx.ETSCronFlusherTest.DefaultPromExSetUp.Metrics_dist)
+  end
+
+  defp get_metrics_table do
+    :ets.tab2list(PromEx.ETSCronFlusherTest.DefaultPromExSetUp.Metrics)
+  end
+
+  defp get_timer_ref do
+    DefaultPromExSetUp.__ets_cron_flusher_name__()
+    |> :sys.get_state()
+    |> Map.get(:timer_ref)
+  end
+end
