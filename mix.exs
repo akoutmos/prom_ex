86921a9958a4174d22459a37ab8014f45a4eae95
Hans Krutzer <git@pixelspaceships.com>
2022-04-07 15:12:11 +0200

    Relax telemetry versioning requirement

--- mix.exs
+++ mix.exs
@@ -47,10 +47,10 @@ defmodule PromEx.MixProject do
       # Required dependencies
       {:jason, "~> 1.2"},
       {:finch, "~> 0.10.2"},
-      {:telemetry, "~> 1.0.0"},
-      {:telemetry_poller, "~> 1.0.0"},
+      {:telemetry, "~> 1.0"},
+      {:telemetry_poller, "~> 1.0"},
       {:telemetry_metrics, "~> 0.6.1"},
-      {:telemetry_metrics_prometheus_core, "~> 1.0.2"},
+      {:telemetry_metrics_prometheus_core, "~> 1.0"},
       {:plug_cowboy, "~> 2.5.1"},
 
       # Optional dependencies depending on what telemetry events the user is interested in capturing
