d76956385c5622096c88b33545c3998f7cc16de8
Troy Martin <troy@devato.com>
2022-04-11 12:18:22 -0400

    Use topology name for message and exception tags (#134)

--- test/prom_ex/plugins/broadway_test.exs
+++ test/prom_ex/plugins/broadway_test.exs
@@ -1,8 +1,16 @@
 defmodule PromEx.Plugins.BroadwayTest do
   use ExUnit.Case, async: true
 
+  alias PromEx.MetricTypes.Event
   alias PromEx.Plugins.Broadway
   alias PromEx.Test.Support.{Events, Metrics}
+  alias Telemetry.Metrics.Distribution
+
+  @default_metadata %{
+    processor_key: :default,
+    topology_name: Elixir.SomeModule,
+    message: %Elixir.Broadway.Message{acknowledger: {Elixir.SomeAcker}, data: %{}}
+  }
 
   defmodule WebApp.PromEx do
     use PromEx, otp_app: :web_app
@@ -23,5 +31,29 @@ defmodule PromEx.Plugins.BroadwayTest do
       |> Metrics.sort()
 
     assert metrics == Metrics.read_expected(:broadway)
+  end
+
+  describe "event_metrics/1" do
+    test "returns topology_name for message tags" do
+      metric = assert_event_metric(:broadway_message_event_metrics, [:broadway, :processor, :message, :stop])
+      assert %{name: "SomeModule", processor_key: :default} = metric.tag_values.(@default_metadata)
+    end
+
+    test "returns topology_name for message exception tags" do
+      metric = assert_event_metric(:broadway_message_event_metrics, [:broadway, :processor, :message, :exception])
+      exception_metadata = Map.merge(@default_metadata, %{kind: Error, reason: "notsure", stacktrace: []})
+      assert %{name: "SomeModule", processor_key: :default} = metric.tag_values.(exception_metadata)
+    end
+
+    defp assert_event_metric(metric_group, event) do
+      assert event_metrics = Broadway.event_metrics(otp_app: :web_app)
+
+      assert %Event{metrics: message_metrics} =
+               Enum.find(event_metrics, fn metrics -> metrics.group_name == metric_group end)
+
+      assert %Distribution{} = metric = Enum.find(message_metrics, fn dist -> dist.event_name == event end)
+
+      metric
+    end
   end
 end
