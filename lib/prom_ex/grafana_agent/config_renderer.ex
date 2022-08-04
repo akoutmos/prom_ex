ee34c30eb86538a6708c48f0c937e039db9026a8
Pat Maddox <pat@ratiopbc.com>
2022-08-03 17:35:53 -0700

    Allow overriding the template file

--- lib/prom_ex/grafana_agent/config_renderer.ex
+++ lib/prom_ex/grafana_agent/config_renderer.ex
@@ -9,17 +9,20 @@ defmodule PromEx.GrafanaAgent.ConfigRenderer do
   """
   @spec generate_config_file(opts :: map(), config_dir :: String.t()) :: String.t()
   def generate_config_file(opts, config_dir) do
-    template_config =
-      :prom_ex
-      |> :code.priv_dir()
-      |> List.to_string()
-      |> Path.join("/grafana_agent/default_config.yml.eex")
-      |> File.read!()
-
+    template_config = File.read!(template_file(opts))
     rendered_config = EEx.eval_string(template_config, assigns: opts)
     config_file_path = Path.join(config_dir, "agent.yml")
     File.write(config_file_path, rendered_config)
 
     config_file_path
+  end
+
+  defp template_file(%{template_file: file}), do: file
+
+  defp template_file(_) do
+    :prom_ex
+    |> :code.priv_dir()
+    |> List.to_string()
+    |> Path.join("/grafana_agent/default_config.yml.eex")
   end
 end
