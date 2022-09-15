2e4c64600f3b2f210c0008f2eafbb140320797a8
Pat Maddox <pat@ratiopbc.com>
2022-09-15 12:30:43 -0700

    Remove unnecessary test

--- test/prom_ex/config_test.exs
+++ test/prom_ex/config_test.exs
@@ -46,18 +46,6 @@ defmodule PromEx.ConfigTest do
              } = config
     end
 
-    test "should include finch_pools if provided" do
-      config =
-        Config.build(
-          grafana: [
-            host: "https://my-grafana-instance.com/",
-            finch_pools: %{default: [size: 1]}
-          ]
-        )
-
-      assert config.grafana_config.finch_pools == %{default: [size: 1]}
-    end
-
     test "should generate default grafana agent config" do
       config =
         Config.build(
